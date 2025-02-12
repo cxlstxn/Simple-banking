bafde5362ca04128c621304bf822948220773026
Aljo Meledom <aljomeledom@Aljos-MacBook-Pro.local>
2025-02-12 16:03:35 +0000

    Saving local changes before pulling

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -1,14 +1,22 @@
 package uk.co.asepstrath.bank;
 
 public class Account {
+    private int balance;
+
+    public Account() {
+        balance = 0;
+    }
 
 
     public void deposit(int amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
 
     }
 
     public int getBalance() {
-        return 0;
+        return balance;
     }
 
 }
