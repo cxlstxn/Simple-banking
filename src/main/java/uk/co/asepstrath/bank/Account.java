6e29a6b46b24bc66134d7a8c657eb0ee27b2e5f7
Kaiss Alsalabi <kaiss.alsalabi.2022@uni.strath.ac.uk>
2025-02-13 13:51:47 +0000

    removed redundancy between with having both startingBalance & balance, leaving only balance

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -4,25 +4,28 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 public class Account {
-    private int balance = 0;
-
-    private UUID id;
+    private BigDecimal balance;
+    private final UUID id;
     private String name;
-    private BigDecimal startingBalance;
     private boolean roundUpEnabled;
 
-    public Account(UUID id, String name, BigDecimal startingBalance, boolean roundUpEnabled) {
+    public Account(UUID id, String name, BigDecimal balance, boolean roundUpEnabled) {
         this.id = id;
         this.name = name;
-        this.startingBalance = startingBalance;
+        this.balance = balance;
         this.roundUpEnabled = roundUpEnabled;
     }
+
     public void deposit(int amount) {
-        startingBalance = startingBalance.add(BigDecimal.valueOf(amount));
+        balance = balance.add(BigDecimal.valueOf(amount));
+    }
+
+    public void withdraw(int amount) {
+        balance = balance.subtract(BigDecimal.valueOf(amount));
     }
 
     public int getBalance() {
-        return startingBalance.intValue();
+        return balance.intValue();
     }
 
 }
