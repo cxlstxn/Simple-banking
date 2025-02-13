eb782e76b980dc2d34893eeef15a24c2dd6c8606
Kaiss Alsalabi <kaiss.alsalabi.2022@uni.strath.ac.uk>
2025-02-13 13:52:12 +0000

    added withdraw20 test

--- src/test/java/uk/co/asepstrath/bank/AccountTests.java
+++ src/test/java/uk/co/asepstrath/bank/AccountTests.java
@@ -3,17 +3,20 @@ package uk.co.asepstrath.bank;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 public class AccountTests {
 
     @Test
     public void createAccount(){
-        Account a = new Account();
+        Account a = new Account(UUID.randomUUID(), "TestCreateAccount", new BigDecimal(0), false);
         Assertions.assertTrue(a != null);
     }
 
     @Test
     public void withdraw20(){
-        Account a = new Account();
+        Account a = new Account(UUID.randomUUID(), "Kaiss", new BigDecimal(0), false);
         a.deposit(40);
         a.withdraw(20);
         Assertions.assertTrue(a.getBalance() == 20);
