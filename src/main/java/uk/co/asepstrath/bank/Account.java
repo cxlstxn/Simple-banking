7c45efbff70cea03083379d52390c137e41e2660
Ariyan Chowdhury <ariyan.chowdhury.2023@uni.strath.ac.uk>
2025-02-14 14:47:40 +0000

    Added a constructor in the Account.java for creating a new account with just a name and also one with name and balance (commented out). added enable and disable roundup methods. Fixed the issues with needing to put strings into the constructor. Added the toString method and added a test for it. cleaned up tests

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -9,26 +9,55 @@ public class Account {
     private String name;
     private boolean roundUpEnabled;
 
-    public Account(UUID id, String name, BigDecimal balance, boolean roundUpEnabled) {
+    public Account(UUID id, String name, double balance, boolean roundUpEnabled) { // This is for when the api is putting in data for an account that already exists
         this.id = id;
         this.name = name;
-        this.balance = balance;
+        this.balance = BigDecimal.valueOf(balance);
         this.roundUpEnabled = roundUpEnabled;
     }
 
-    public void deposit(BigDecimal amount) {
-        balance = balance.add(amount);
+    public Account(String name) { // This is for creating a new account
+        this(UUID.randomUUID(), name, 0, false);
     }
 
-    public void withdraw(BigDecimal amount) {
-        if(amount.compareTo(balance) > 0){
+    /* This may be useful if a customer wants to create an account and already wants to put money in it
+       Commented out as a customer can currently create an account and just then deposit initial amount
+
+    public Account(String name, double balance) {
+        this(UUID.randomUUID(), name, balance, false);
+    }
+     */
+
+    public void deposit(double amount) {
+        balance = balance.add(BigDecimal.valueOf(amount));
+    }
+
+    public void withdraw(double amount) {
+        BigDecimal amountInBigDecimal = BigDecimal.valueOf(amount);
+        if(amountInBigDecimal.compareTo(balance) > 0){
             throw new ArithmeticException("Insufficient funds");
         }
-        balance = balance.subtract(amount);
+        balance = balance.subtract(amountInBigDecimal);
     }
 
+    public void enableRoundUp() {
+    if (!roundUpEnabled) {
+        roundUpEnabled = true;
+    }
+    }
+    public void disableRoundUp() {
+        if (roundUpEnabled) {
+            roundUpEnabled = false;
+        }
+    }
     public BigDecimal getBalance() {
         return balance;
     }
 
+    @Override
+    public String toString() {
+        return "Name: " + name + " | Balance: " + balance;
+        //return "ID: " + id + " | Name: " + name + " | Balance: " + balance + " | Round up: " + roundUpEnabled;
+        //Not too sure what to put into the output string
+    }
 }
--- src/test/java/uk/co/asepstrath/bank/AccountTests.java
+++ src/test/java/uk/co/asepstrath/bank/AccountTests.java
@@ -10,53 +10,61 @@ public class AccountTests {
 
     @Test
     public void createAccount(){
-        Account a = new Account(UUID.randomUUID(), "TestCreateAccount", new BigDecimal(0), false);
-        Assertions.assertTrue(a != null);
+        Account a = new Account(UUID.randomUUID(), "TestCreateAccount", 0, false);
+        Account b = new Account("TestCreateAccount");
+        Assertions.assertNotNull(a);
+        Assertions.assertNotNull(b);
     }
 
     @Test   // Balance when a new account is created should be 0
     public void checkInitialBalance(){
-        Account a = new Account(UUID.randomUUID(), "TestCheckInitialBalance", new BigDecimal(0), false);
-        Assertions.assertEquals(a.getBalance(), BigDecimal.valueOf(0));
+        Account a = new Account("TestCheckInitialBalance"); // when a new account is created then the name only needs to be inputted
+        Assertions.assertEquals(BigDecimal.valueOf(0.0), a.getBalance());
     }
 
     @Test   // Depositing £50 in an account with £20 should result in an account containing £70
     public void depositFunds(){
-        Account a = new Account(UUID.randomUUID(), "TestDepositFunds", new BigDecimal(20), false);
-        a.deposit(BigDecimal.valueOf(50));
-        Assertions.assertEquals(a.getBalance(), BigDecimal.valueOf(70));
+        Account a = new Account(UUID.randomUUID(), "TestDepositFunds", 20, false);
+        a.deposit(50);
+        Assertions.assertEquals(BigDecimal.valueOf(70.0), a.getBalance());
     }
 
     @Test   // Withdrawing £20 from an account with £40 should result in an account containing £20
     public void withdrawFunds(){
-        Account a = new Account(UUID.randomUUID(), "TestWithdrawFunds", new BigDecimal(40), false);
-        a.withdraw(BigDecimal.valueOf(20));
-        Assertions.assertEquals(a.getBalance(), (BigDecimal.valueOf(20)));
+        Account a = new Account(UUID.randomUUID(), "TestWithdrawFunds", 40, false);
+        a.withdraw(20);
+        Assertions.assertEquals(BigDecimal.valueOf(20.0), a.getBalance());
     }
 
     @Test   // Withdrawing £100 from an account with £30 should throw an ArithmeticException
     public void overflowWithdrawal(){
-        Account a = new Account(UUID.randomUUID(), "TestOverflowWithdrawal", new BigDecimal(30), false);
-        Assertions.assertThrows(ArithmeticException.class, () -> a.withdraw(BigDecimal.valueOf(100)));
-        Assertions.assertEquals(a.getBalance(), BigDecimal.valueOf(30)); // Check to see if amount changes
+        Account a = new Account(UUID.randomUUID(), "TestOverflowWithdrawal", 30, false);
+        Assertions.assertThrows(ArithmeticException.class, () -> a.withdraw(100));
+        Assertions.assertEquals(BigDecimal.valueOf(30.0), a.getBalance()); // Check to see if amount changes
     }
 
    @Test   // Starting with an account with £20, deposit £10 five times then withdraw £20 three times. The account should end with £10
    public void depositAndWithdraw(){
-       Account a = new Account(UUID.randomUUID(), "TestDepositAndWithdraw", new BigDecimal(20), false);
+       Account a = new Account(UUID.randomUUID(), "TestDepositAndWithdraw", 20, false);
        for (int i = 0; i < 5; i++){
-           a.deposit(BigDecimal.valueOf(10));
+           a.deposit(10);
        }
        for (int i = 0; i < 3; i++){
-           a.withdraw(BigDecimal.valueOf(20));
+           a.withdraw(20);
        }
-       Assertions.assertEquals(a.getBalance(), BigDecimal.valueOf(10));
+       Assertions.assertEquals(BigDecimal.valueOf(10.0), a.getBalance());
    }
 
    @Test    // Depositing £17.56 into an account with £5.45 should result in an account containing £23.01
     public void depositPennies(){
-       Account a = new Account(UUID.randomUUID(), "TestDepositAndWithdraw", new BigDecimal("5.45"), false); // <- Late, Try find a way to not pass a string literal and have it still work
-       a.deposit(BigDecimal.valueOf(17.56));
-       Assertions.assertEquals(a.getBalance(), BigDecimal.valueOf(23.01));
+       Account a = new Account(UUID.randomUUID(), "TestDepositAndWithdraw", 5.45, false);
+       a.deposit(17.56);
+       Assertions.assertEquals(BigDecimal.valueOf(23.01), a.getBalance());
+   }
+
+   @Test
+    public void stringAccount(){  //Testing override of .toString() method to return a string with the account name and balance
+        Account a = new Account(UUID.randomUUID(), "TestStringAccount", 10, false);
+        Assertions.assertEquals("Name: TestStringAccount | Balance: 10.0", a.toString());
    }
 }
